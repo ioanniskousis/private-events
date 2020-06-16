bc15fc362ccde1a531ab7306224edc792f0341cd
Ioannis Kousis <bonobo@otenet.gr>
2020-06-16 12:48:25 +0300

    Added sessions controller and user validation

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+
+  def new
+
+    render :new
+  end
+
+  def create
+    @user = User.find_by username: params[:username]
+
+    if @user
+      redirect_to user_path(@user) 
+    else
+      flash.notice = "'" + params[:username] + "' : is a Wrong User Name !!" 
+
+      redirect_to new_session_path
+    end
+  end
+
+  def destroy
+
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_16_082744) do
+ActiveRecord::Schema.define(version: 2020_06_16_084343) do
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "username"
   end
 
 end
